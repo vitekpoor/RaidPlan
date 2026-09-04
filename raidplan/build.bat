4bf3519ec3ac0f321862a8484a8e66a36458f583
Vítek Poór <vitek.poor@seznam.cz>
2026-09-04 12:26:48 +0200

    Update raid guide

--- raidplan/build.bat
+++ raidplan/build.bat
@@ -15,7 +15,7 @@ type parts\head.html > venomous-abyss.html
 for %%f in (bosses\*.html) do type "%%f" >> venomous-abyss.html
 type parts\tail.html >> venomous-abyss.html
 
-powershell -NoProfile -Command "[IO.File]::WriteAllText('%~dp0..\index.html', ((Get-Content -Raw -Encoding UTF8 '%~dp0venomous-abyss.html') -replace 'boss_2_marks\.png', 'raidplan/boss_2_marks.png'), (New-Object System.Text.UTF8Encoding $false))"
+powershell -NoProfile -Command "[IO.File]::WriteAllText('%~dp0..\index.html', ((Get-Content -Raw -Encoding UTF8 '%~dp0venomous-abyss.html') -replace '(boss_2_marks|sszorak_addon_0\d)\.png', 'raidplan/$0'), (New-Object System.Text.UTF8Encoding $false))"
 if errorlevel 1 (echo ERROR: failed to write ..\index.html & exit /b 1)
 
 echo Built venomous-abyss.html + ..\index.html from parts\ and bosses\.
